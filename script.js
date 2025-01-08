738d56809e7a1b3137a6349751aa8a2499d415ad
Kai Kameyama <1203kk06@gmail.com>
2025-01-07 21:27:40 -0800

    intial commit

---
+++ script.js
@@ -0,0 +1,158 @@
+const Gameboard = (function () {
+    const board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]    
+    ];
+
+    function placePick(rowNum, colNum, symbol) {
+        if (board[rowNum][colNum] !== null) {
+            return false;
+        } else {
+            board[rowNum][colNum] = symbol;
+        }
+        return true;
+    }
+
+    function getBoard() {
+        return board;
+    }
+
+    function resetBoard() {
+        for (let row = 0; row < board.length; row++) {
+            for (let col = 0; col < board[row].length; col++) {
+                board[row][col] = null;
+            }
+        }
+    }
+
+    function checkWinner(symbol) {
+
+        for (let row of board) {
+            if (row.every(cell => cell === symbol)) return true;
+        }
+
+
+        for (let col = 0; col < board.length; col++) {
+            if (board.every(row => row[col] === symbol)) {
+                return true;
+            }
+        }
+
+
+        if (board.every((row, idx) => row[idx] === symbol)) {
+            return true;
+        }
+
+
+        if (board.every((row, idx) => row[board.length - 1 - idx] === symbol)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function isTie() {
+        return board.every(row => row.every(cell => cell !== null));
+    }
+
+    return { placePick, getBoard, resetBoard, checkWinner, isTie };
+})();
+
+// Gamecontroller module
+const Gamecontroller = (function() {
+    let currentPlayer = "X";
+    let gameOver = false; 
+
+    function playTurn(rowNum, colNum) {
+        if (gameOver) {
+            alert("Game over! Click reset to start a new game.");
+            return false;
+        }
+
+        if (Gameboard.placePick(rowNum, colNum, currentPlayer) === false) {
+            alert("This position has already been taken");
+            return false;
+        }
+
+
+        const tiles = document.querySelectorAll(".tile");
+        const index = rowNum * 3 + colNum;
+        tiles[index].textContent = currentPlayer;
+
+
+        if (Gameboard.checkWinner(currentPlayer)) {
+            console.log(`${currentPlayer} wins!`);
+            gameOver = true;
+            return true;
+        }
+
+        
+        if (Gameboard.isTie()) {
+            console.log("It's a tie!");
+            gameOver = true; 
+            return true;
+        }
+
+        
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        return true;
+    }
+
+    function resetGame() {
+        Gameboard.resetBoard();
+        currentPlayer = "X";
+        gameOver = false; 
+
+        const tiles = document.querySelectorAll(".tile");
+        tiles.forEach(tile => {
+            tile.textContent = ""; 
+        });
+    }
+
+    function getCurrentPlayerSymbol() {
+        return currentPlayer;
+    }
+
+    function getGameOver() {
+        return gameOver; 
+    }
+
+    return { playTurn, resetGame, getCurrentPlayerSymbol, getGameOver };
+})();
+
+
+const Displaycontainer = (function () {
+    const init = (tiles) => {
+        tiles.forEach(tile => {
+            tile.addEventListener("click", () => {
+                const index = tile.dataset.index;
+                const row = Math.floor(index / 3);
+                const col = index % 3;
+
+                if (Gamecontroller.playTurn(row, col)) {
+                    if (Gamecontroller.getGameOver()) {
+                        tiles.forEach(t => t.removeEventListener("click", () => {})); 
+                    }
+                }
+            });
+        });
+    };
+
+    const resetBoard = (tiles) => {
+        tiles.forEach(tile => (tile.textContent = ""));
+    };
+
+    return { init, resetBoard };
+})();
+
+
+const tiles = document.querySelectorAll(".tile");
+Displaycontainer.init(tiles);
+
+
+const resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", () => {
+    Gamecontroller.resetGame();
+    Displaycontainer.resetBoard(tiles);
+});
